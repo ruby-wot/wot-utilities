60349d98ab6fcb5bd8a658956c3ba2afb13faff3
Marie Markwell <me@marie.so>
2014-10-17 00:58:46 -0400

    Fix caller_method.
    Pleasant side-effect: Spinny::Utilities.caller_binding(1) returns the
    binding of the caller's caller.

--- lib/spinny/utilities.rb
+++ lib/spinny/utilities.rb
@@ -3,12 +3,12 @@ require 'debug_inspector'
 
 module Spinny
   module Utilities
-    def self.caller_binding
-      RubyVM::DebugInspector.open {|i| i.frame_binding(3) }
+    def self.caller_binding(offset=0)
+      RubyVM::DebugInspector.open {|i| i.frame_binding(3 + offset) }
     end
 
-    def self.caller_method
-      Spinny::Utilities.caller_binding.eval('method(__method__)')
+    def self.caller_method(offset=0)
+      Spinny::Utilities.caller_binding(1 + offset).eval('method(__method__)')
     end
   end
 end
