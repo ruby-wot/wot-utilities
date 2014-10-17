a488baaddd88d70c709e3e4390c17dd4c06325ff
Marie Markwell <me@marie.so>
2014-10-17 00:56:31 -0400

    Add Spinny::Utilities.caller_method.

--- lib/spinny/utilities.rb
+++ lib/spinny/utilities.rb
@@ -6,5 +6,9 @@ module Spinny
     def self.caller_binding
       RubyVM::DebugInspector.open {|i| i.frame_binding(3) }
     end
+
+    def self.caller_method
+      Spinny::Utilities.caller_binding.eval('method(__method__)')
+    end
   end
 end
